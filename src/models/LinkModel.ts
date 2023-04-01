254ea3ecbc468724d0dd6305cd587d10818d5c41
Jeffrey Cable <jeffrey.cable1@smail.astate.edu>
2023-03-31 20:49:50 -0500

    Generating LinkId complete

--- src/models/LinkModel.ts
+++ src/models/LinkModel.ts
@@ -1,3 +1,4 @@
+import { createHash } from 'crypto';
 import { Link } from '../entities/Link';
 import { AppDataSource } from '../dataSource';
 
@@ -8,4 +9,13 @@ async function getLinkById(linkId: string): Promise<Link | null> {
   return link;
 }
 
-export { getLinkById };
+function createLinkId(originalUrl: string, userId: string): string {
+  const md5 = createHash('md5');
+  md5.update(originalUrl + userId);
+  const urlHash = md5.digest('base64url');
+  const linkId = urlHash.slice(0, 8);
+
+  return linkId;
+}
+
+export { getLinkById, createLinkId };
